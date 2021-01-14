6bbe66d32a28425212c09f828dca2a3cafd9b1ff
tigreal <fosmaralvarez@gmail.com>
2021-01-14 13:32:15 -0400

    Learn_Python_in_One_Day_and_Learn_It_Well

---
+++ fileOperation.py
@@ -0,0 +1,10 @@
+f=open('myfile.txt','r')
+firstline = f.readline()
+
+secondline = f.readline()
+
+print (firstline)
+
+print (secondline)
+f.close()
+
---
+++ fileOperationWithFoorLoop.py
@@ -0,0 +1,9 @@
+#leer un archivo de testo con un for loop
+f=open('myfile.txt','r')
+for lines in f:
+    print(lines)
+f.close()
+    
+    
+
+
